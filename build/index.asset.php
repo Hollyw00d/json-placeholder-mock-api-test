d00e4a09ecc0e011928f6226b43e55704af6542e
Matt Jennings <matt@mattjennings.net>
2025-01-20 14:14:08 -0800

    converted back to npm from pnpm

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => 'e7ff02bd46415332d65b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '4de6954c1579d217c2c2');
--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-dom-ready', 'wp-element'), 'version' => '2ddaa22a6d5a41d01ea0');
+<?php return array('dependencies' => array('react-dom', 'wp-dom-ready', 'wp-element'), 'version' => '72b4ae4a9f5ac298a006');
