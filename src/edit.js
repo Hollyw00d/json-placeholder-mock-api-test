c9f2809e4e7d7422680f795f3813d48336f34218
Matt Jennings <matt@mattjennings.net>
2025-02-02 17:21:18 -0800

    updated edit.js and eslint config to fix rule issues

--- src/edit.js
+++ src/edit.js
@@ -1,15 +1,16 @@
-/* eslint-disable react/jsx-filename-extension */
-import { useBlockProps } from '@wordpress/block-editor'; // eslint-disable-line import/no-unresolved
-import App from './components/App/App';
+import { useBlockProps } from '@wordpress/block-editor';
+import App from './components/App/App.jsx'; // eslint-disable-line import/extensions
 
 import './editor.scss';
 
 export default function Edit() {
 	const isEditPage = true;
 
+	/* eslint-disable react/jsx-filename-extension */
 	return (
 		<div {...useBlockProps()}>
 			<App isEditPage={isEditPage} />
 		</div>
 	);
+	/* eslint-enable react/jsx-filename-extension */
 }
